3d68170ed5013fe634c19fed7a7ebb9602f00efa
Tom-Brd <bourlard.tom.94@gmail.com>
2023-04-25 12:30:04 +0200

    config the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
+
+  # Pokemon routes
+  get '/pokemon', to: 'pokemon#index'
+  get '/pokemon/:id', to: 'pokemon#show'
+  get '/pokemon/:id/checkout', to: 'pokemon#checkout'
+  post '/pokemon/:id/buy', to: 'pokemon#buy'
+  post '/pokemon/:id/sell', to: 'pokemon#sell'
+
+  # Users routes
+  get '/me', to: 'user#show'
+  get '/me/add', to: 'user#add_money_form'
+  patch '/me/add', to: 'user#add_money'
+get '/me/transactions', to: 'user#transactions'
+
+
 end
